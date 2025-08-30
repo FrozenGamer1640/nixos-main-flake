e929a0dab84bc9b4a793105eb663c4ac1c43a1bb
FrozenGamer1640 <gamercfp@gmail.com>
2025-08-29 20:30:06 -0600

    vscode config changes

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -9,13 +9,13 @@
     #config = mkIf cfg.enable {
       programs.vscode = {
         enable = true;
-        profiles.default.extensions = with pkgs.vscode-extensions; [
-          bbenoist.nix
-          jnoortheen.nix-ide
-          shardulm94.trailing-spaces
-          catppuccin.catppuccin-vsc
-          catppuccin.catppuccin-vsc-icons
-        ];
+        #profiles.default.extensions = with pkgs.vscode-extensions; [
+        #  bbenoist.nix
+        #  jnoortheen.nix-ide
+        #  shardulm94.trailing-spaces
+        #  catppuccin.catppuccin-vsc
+        #  catppuccin.catppuccin-vsc-icons
+        #];
       };
 
       home.file.".config/Code/User/settings.json".source = ./settings.json;
