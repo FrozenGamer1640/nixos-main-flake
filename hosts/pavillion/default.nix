02d06614762ef5d54f78870d8d2f98d61dc4f519
FrozenGamer1640 <gamercfp@gmail.com>
2025-10-28 19:09:38 -0600

    reorganized nixos pavillion settings and modules

--- hosts/pavillion/default.nix
+++ hosts/pavillion/default.nix
@@ -1,33 +1,48 @@
 { inputs, pkgs, ezModules, ... }:
 {
-  imports = [
+  imports = with ezModules; [
     ./hardware-configuration.nix
-    ezModules.pipewire
+    pipewire locale-es-cr
   ];
+
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "25.05";
 
+  nix.settings.allowed-users = [ "frozenfox" ];
   users.users.frozenfox = {
     isNormalUser = true;
     extraGroups = [ "input" "wheel" "networkmanager" ];
   };
 
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-      systemd-boot.editor = false;
-      efi.canTouchEfiVariables = true;
-      timeout = 25;
-    };
-  };
-
   services = {
     upower.enable = true;
+    dbus.enable = true;
+    playerctld.enable = true;
+    hypridle.enable = true;
     gnome.gnome-keyring.enable = true;
 
     displayManager = {
       cosmic-greeter.enable = true;
     };
+
+    desktopManager = {
+      cosmic = {
+        enable = true;
+        xwayland.enable = true;
+      };
+
+    };
+  };
+
+  xdg.portal = {
+    enable = true;
+    config.common.default = [ "hyprland" ];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-cosmic
+    ];
+    configPackages = with pkgs; [
+      xdg-desktop-portal-hyprland
+    ];
   };
 
   programs = {
@@ -46,57 +61,16 @@
     sessionVariables = {
       NIXOS_CONFIG = "$HOME/.config/nixos/configuration.nix";
       NIXOS_CONFIG_DIR = "$HOME/.config/nixos/";
-      # XDG_DATA_HOME = "$HOME/.local/share";
+      NIXOS_OZONE_WL = "1";
       PASSWORD_STORE_DIR = "$HOME/.local/share/password-store";
       EDITOR = "zeditor";
-      NIXOS_OZONE_WL = "1";
     };
-  };
-
-  xdg.portal = {
-    enable = true;
-    config.common.default = [ "hyprland" ];
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-cosmic
-    ];
-    configPackages = with pkgs; [
-      xdg-desktop-portal-hyprland
-    ];
-  };
-
-  nix.settings.allowed-users = [ "frozenfox" ];
-
-  time.timeZone = "America/Costa_Rica";
-  i18n = {
-    defaultLocale = "es_MX.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "es_CR.UTF-8";
-      LC_IDENTIFICATION = "es_CR.UTF-8";
-      LC_MEASUREMENT = "es_CR.UTF-8";
-      LC_MONETARY = "es_CR.UTF-8";
-      LC_NAME = "es_CR.UTF-8";
-      LC_NUMERIC = "es_CR.UTF-8";
-      LC_PAPER = "es_CR.UTF-8";
-      LC_TELEPHONE = "es_CR.UTF-8";
-      LC_TIME = "es_CR.UTF-8";
-    };
-  };
-
-  console = {
-    #font = "JetBrainsMono";
-    keyMap = "la-latin1";
   };
 
   networking = {
-    #wireless.iwd.enable = true;
     networkmanager.enable = true;
-    hostName = "FrozenFox";
-    firewall = {
-      enable = true;
-      #allowedTCPPorts = [ 443 80 ];
-      #allowedUDPPorts = [ 443 80 44857 ];
-      #allowPing = false;
-    };
+    hostName = "Fuyuka's Pavillion Laptop";
+    firewall.enable = true;
   };
 
   security.rtkit.enable = true;
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,5 +1,14 @@
 { ... }:
 {
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      systemd-boot.editor = false;
+      efi.canTouchEfiVariables = true;
+      timeout = 25;
+    };
+  };
+
   nix = {
     settings.auto-optimise-store = true;
     gc = {
