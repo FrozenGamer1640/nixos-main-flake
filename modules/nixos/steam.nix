79fc71f8742445f58c743846de2a9dcc4855b198
FrozenGamer1640 <gamercfp@gmail.com>
2025-11-26 07:42:37 -0600

    Fixed steam nixos module as i could

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -1,13 +1,50 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    protontricks.enable = true;
-    gamescopeSession = {
+  programs = {
+    appimage = {
       enable = true;
+      binfmt = true;
+    };
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+      env = {
+        __VK_LAYER_NV_optimus = "NVIDIA_only";
+        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+        MANGOHUD = "1";
+      };
+    };
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      # protontricks.enable = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
+      extraPackages = with pkgs; [
+        gamescope
+      ];
+      gamescopeSession = {
+        enable = true;
+        args = [
+          "--adaptive-sync"
+          "--hdr-enabled"
+          "--mangoapp"
+          "--rt"
+          "--steam"
+        ];
+        steamArgs = [
+          "-pipewire-dmabuf"
+          "-tenfoot"
+        ];
+      };
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    steam-run
+    mangohud
+  ];
 }
