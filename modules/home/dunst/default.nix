caaf93c4b0d2597c60c07b24a09743d08bb7edf5
FrozenGamer1640 <gamercfp@gmail.com>
2025-11-27 14:40:52 -0600

    tweaked dunst module to use stylix

--- modules/home/dunst/default.nix
+++ modules/home/dunst/default.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ lib, ... }:
 {
+  stylix.targets.dunst.enable = true;
   services.dunst = {
     enable = true;
     settings = {
-      global = {
+      global = lib.mkForce {
         origin = "top-left";
         offset = "60x12";
         separator_height = 2;
@@ -11,9 +12,7 @@
         horizontal_padding = 12;
         text_icon_padding = 12;
         frame_width = 4;
-        separator_color = "frame";
         idle_threshold = 120;
-        font = "JetBrainsMono Nerdfont 12";
         line_height = 0;
         format = "<b>%s</b>\n%b";
         alignment = "center";
@@ -21,9 +20,6 @@
         startup_notification = "false";
         corner_radius = 12;
 
-        frame_color = "#44465c";
-        background = "#303241";
-        foreground = "#d9e0ee";
         timeout = 2;
       };
     };
