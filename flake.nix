1de051a988f158ce55d3f9ad7266eccf02ca6a5e
Fuyuka.nix <fuyuka.nix@proton.me>
2025-12-07 20:04:05 -0600

    added lazy main flake structure

--- flake.nix
+++ flake.nix
@@ -1,13 +1,22 @@
 {
   inputs = {
-
+    hosts = {
+      type = "path";
+      path = "./hosts";
+    };
+    users = {
+      type = "path";
+      path = "./users";
+    };
   };
 
   outputs =
     {
-      ...
+      hosts,
+      users,
     }:
     {
-
+      inherit (hosts) nixosConfigurations;
+      inherit (users) homeConfigurations;
     };
 }
