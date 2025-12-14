45123da71769ef364626f44496a6e286675d1658
Fuyuka.nix <fuyuka.nix@proton.me>
2025-12-13 21:15:10 -0600

    added something i forgot in the xdg module hehe

--- modules/home/xdg/default.nix
+++ modules/home/xdg/default.nix
@@ -7,15 +7,15 @@
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
+    download = "${config.home.homeDirectory}/stuff/Downloads";
   }
   // lib.genAttrs [
     "documents"
-    "download"
     "videos"
     "music"
     "pictures"
     "desktop"
     "publicShare"
     "templates"
-  ] (dir: "${config.home.homeDirectory}/${lib.toSentenceCase dir}");
+  ] (dir: "${config.home.homeDirectory}/stuff/${lib.toSentenceCase dir}");
 }
