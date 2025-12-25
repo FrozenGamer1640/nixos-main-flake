a7e505510093d19aef486f0d63dc6e5777172cab
Fuyuka.nix <fuyuka.nix@proton.me>
2025-12-24 20:31:18 -0600

    added a bit of neovim QoL customization

--- environments/hyprlazer-desk/home/neovim/default.nix
+++ environments/hyprlazer-desk/home/neovim/default.nix
@@ -1,15 +1,47 @@
 {
   pkgs,
+  lib,
   ...
 }:
+let
+  tabWidth = "4";
+in
 {
   programs.neovim = {
     enable = true;
-    extraConfig =
-    ''
-    	set number
-	set relativenumber
-    '';
+    extraConfig = lib.concatStringsSep "\n" (lib.concatLists [
+      (lib.forEach [
+        "number"
+        "relativenumber"
+
+        "shiftwidth=${tabWidth}"
+        "tabstop=${tabWidth}"
+        "softtabstop=${tabWidth}"
+        "expandtab"
+        "autoindent"
+        "fileformat=unix"
+
+        "cursorlineopt=number"
+        "cursorline"
+        "wildmode=noselect:full"
+        "splitbelow splitright"
+      ] (x: "set ${x}"))
+
+      (lib.forEach [
+        "h"
+        "j"
+        "k"
+        "l"
+      ] (x: "map <C-${x}> <C-w>${x}"))
+
+      (lib.forEach [
+        "S :%s//g<Left><Left>"
+      ] (x: "nnoremap ${x}"))
+
+      (lib.forEach [
+        "* %s/\\s\\+$//e"
+      ] (x: "autocmd BufWritePre ${x}"))
+    ]);
     plugins = with pkgs.vimPlugins; [
       telescope-nvim
       nvim-colorizer-lua
@@ -19,5 +51,6 @@
       nix
       hyprlang
     ]);
+
   };
 }
